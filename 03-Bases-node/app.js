48d99f80a8cc3e0b7001ad1dd0ca805c6b8f4389
edme88 <edmealiciardi@gmail.com>
2022-03-02 01:19:40 -0300

    Video 34/273

--- 03-Bases-node/app.js
+++ 03-Bases-node/app.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
+const argv = require('yargs').argv;
 const { crearArchivo } = require('./helpers/multiplicarAsync');
 
 
 console.clear();
 
-const [, , arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+//const [, , arg3 = 'base=5'] = process.argv;
+//const [, base = 5] = arg3.split('=');
 
+console.log(process.argv)
+console.log(argv)
 
-crearArchivo(base)
-    .then(nombreArchivo => console.log(nombreArchivo, ' creado'))
-    .catch(err => console.log(err));
+console.log('base: yargs', argv.base)
+
+//crearArchivo(base)
+//    .then(nombreArchivo => console.log(nombreArchivo, ' creado'))
+//    .catch(err => console.log(err));
